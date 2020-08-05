f07a0abb1a5e8580ac0a811ba5aa67356f2b262d
yoel <yoel@macbook-pro.home>
2020-08-05 15:27:46 -0700

    [MVC-Deliverable] Add Rename

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 const express = require("express");
 const app = express();
 
-// express.static is a built-in middleware function in express the format is express.static(root, [options]), so the root is your particular root directory (in this case __dirname is always the directory in which the currently executing script resides), like the place from where you'll be serving static assets
-
-app.use(express.static(`${__dirname}/public`));
+// tell express that we're going to use
+// ejs as the view engine
+app.set("view engine", "ejs");
 
 // [GET] home route
 app.get("/", (req, res) => {
-  res.sendFile(`${__dirname}/views/index.html`);
+  // express will look inside views by defualt for an ejs file with this name
+  res.render("index", { name: "Godfrey The Yummy", age: 69 });
+  // res.sendFile(`${__dirname}/views/index.html`);
 });
 
 // [GET] about route
 app.get("/about", (req, res) => {
-  res.sendFile(`${__dirname}/views/about.html`);
+  res.render("about", {
+    fave: ["Chicken Feet", "Wuxia Novels", "Homies of 713"],
+  });
+  // res.sendFile(`${__dirname}/views/about.html`);
 });
 
 // [GET] blog route
 app.get("/blog", (req, res) => {
-  res.sendFile(`${__dirname}/views/blog-directory.html`);
+  res.render("blog-directory");
+  // res.sendFile(`${__dirname}/views/blog-directory.html`);
 });
 
 // listen to these GET'S baby
